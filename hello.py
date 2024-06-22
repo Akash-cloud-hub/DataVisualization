5ce9c2ad6b0747ce8df48cee5407b71d6ddf66fa
Akash <airshula79@gmail.com>
2024-06-22 22:02:33 +0700

    Data visualization line , scatter , bar graphs

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-from flask import flask
+from flask import Flask , render_template , redirect , url_for , request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('hello.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
